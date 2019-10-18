4cd0c53a6a75457a850d8211abd6d72c5f4d5903
Zack Bumpous <upwork.ztb@gmail.com>
2019-10-18 10:23:12 -0500

    just set up my imports that i beleive i will need in my app js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Header from "./components/Header.js";
+import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
+import LocationsList from './components/LocationsList';
+
 
 
 export default function App() {
